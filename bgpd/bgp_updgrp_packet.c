54fd0d01e436e918c0e8a865850caa642f295ad4
Quentin Young <qlyoung@cumulusnetworks.com>
2017-02-06 23:39:06 +0000

    bgpd: move packet writes into dedicated pthread
    * BGP_WRITE_ON() removed
    * BGP_WRITE_OFF() removed
    * peer_writes_on() added
    * peer_writes_off() added
    * bgp_write_proceed_actions() removed
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- bgpd/bgp_updgrp_packet.c
+++ bgpd/bgp_updgrp_packet.c
@@ -633,7 +633,6 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 		}
 	}
 
-	bgp_packet_add(peer, s);
 	return s;
 }
 
@@ -1150,7 +1149,6 @@ void subgroup_default_update_packet(struct update_subgroup *subgrp,
 	bgp_packet_set_size(s);
 
 	(void)bpacket_queue_add(SUBGRP_PKTQ(subgrp), s, &vecarr);
-	subgroup_trigger_write(subgrp);
 }
 
 void subgroup_default_withdraw_packet(struct update_subgroup *subgrp)
@@ -1244,7 +1242,6 @@ void subgroup_default_withdraw_packet(struct update_subgroup *subgrp)
 	bgp_packet_set_size(s);
 
 	(void)bpacket_queue_add(SUBGRP_PKTQ(subgrp), s, NULL);
-	subgroup_trigger_write(subgrp);
 }
 
 static void
