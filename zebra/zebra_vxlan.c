taspelund/frr

commit c4771937e6699ea8ea60ef68d18b6ddebf00a215

zebra: Fix MAC change handling for a neighbor
When the MAC changes for a local neighbor, ensure that the neighbor data
structure as well as the link between the neighbor and MAC data structures
is updated correctly.

Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>
Reviewed-by:   Mitesh Kanjariya <mitesh@cumulusnetworks.com>
Reviewed-by:   Donald Sharp <sharpd@cumulusnetworks.com>

Ticket: CM-17565
Reviewed By: CCR-6605
Testing Done: Manual, evpn-smoke

zebra/zebra_vxlan.c
@@ -2889,10 +2889,9 @@ int zebra_vxlan_local_neigh_add_update(struct interface *ifp,
 	zebra_vni_t *zvni;
 	zebra_neigh_t *n;
 	struct zebra_vrf *zvrf;
-	zebra_mac_t *zmac;
+	zebra_mac_t *zmac, *old_zmac;
 	char buf[ETHER_ADDR_STRLEN];
 	char buf2[INET6_ADDRSTRLEN];
-	int send_upd = 1, send_del = 0;
 
 	/* We are only interested in neighbors on an SVI that resides on top
 	 * of a VxLAN bridge.
@@ -2947,19 +2946,32 @@ int zebra_vxlan_local_neigh_add_update(struct interface *ifp,
 			if (memcmp(n->emac.octet, macaddr->octet,
 				   ETHER_ADDR_LEN)
 			    == 0) {
-				if (n->ifindex == ifp->ifindex)
-					/* we're not interested in whatever has
-					 * changed. */
-					return 0;
-				/* client doesn't care about a purely local
-				 * change. */
-				send_upd = 0;
-			} else
-				/* If the MAC has changed, issue a delete first
-				 * as this means a
-				 * different MACIP route.
+				/* Update any params and return - client doesn't
+				 * care about a purely local change.
 				 */
-				send_del = 1;
+				n->ifindex = ifp->ifindex;
+				return 0;
+			} else {
+				/* If the MAC has changed, need to issue a delete first
+				 * as this means a different MACIP route. Also, need to
+				 * do some unlinking/relinking.
+				 */
+				zvni_neigh_send_del_to_client(zvrf, zvni->vni, &n->ip, &n->emac,
+									      0);
+				old_zmac = zvni_mac_lookup(zvni, &n->emac);
+				if (old_zmac) {
+					listnode_delete(old_zmac->neigh_list, n);
+					zvni_deref_ip2mac(zvni, old_zmac, 0);
+				}
+
+				/* Set "local" forwarding info. */
+				SET_FLAG(n->flags, ZEBRA_NEIGH_LOCAL);
+				n->ifindex = ifp->ifindex;
+				memcpy(&n->emac, macaddr, ETHER_ADDR_LEN);
+
+				/* Link to new MAC */
+				listnode_add_sort(zmac->neigh_list, n);
+			}
 		} else if (ext_learned)
 		/* The neighbor is remote and that is the notification we got.
 		   */
@@ -2986,15 +2998,6 @@ int zebra_vxlan_local_neigh_add_update(struct interface *ifp,
 		}
 	}
 
-	/* Issue delete for older info, if needed. */
-	if (send_del)
-		zvni_neigh_send_del_to_client(zvrf, zvni->vni, &n->ip, &n->emac,
-					      0);
-
-	/* Set "local" forwarding info. */
-	SET_FLAG(n->flags, ZEBRA_NEIGH_LOCAL);
-	n->ifindex = ifp->ifindex;
-
 	/* Before we program this in BGP, we need to check if MAC is locally
 	 * learnt as well */
 	if (!CHECK_FLAG(zmac->flags, ZEBRA_MAC_LOCAL)) {
@@ -3008,21 +3011,17 @@ int zebra_vxlan_local_neigh_add_update(struct interface *ifp,
 		return 0;
 	}
 
-	/* Inform BGP if required. */
-	if (send_upd) {
-		if (IS_ZEBRA_DEBUG_VXLAN)
-			zlog_debug(
-				"%u: neigh %s (MAC %s) is now ACTIVE on VNI %u",
-				ifp->vrf_id, ipaddr2str(ip, buf2, sizeof(buf2)),
-				prefix_mac2str(macaddr, buf, sizeof(buf)),
-				zvni->vni);
+	/* Inform BGP. */
+	if (IS_ZEBRA_DEBUG_VXLAN)
+		zlog_debug(
+			"%u: neigh %s (MAC %s) is now ACTIVE on VNI %u",
+			ifp->vrf_id, ipaddr2str(ip, buf2, sizeof(buf2)),
+			prefix_mac2str(macaddr, buf, sizeof(buf)),
+			zvni->vni);
 
-		ZEBRA_NEIGH_SET_ACTIVE(n);
-		return zvni_neigh_send_add_to_client(zvrf, zvni->vni, ip,
-						     macaddr, 0);
-	}
-
-	return 0;
+	ZEBRA_NEIGH_SET_ACTIVE(n);
+	return zvni_neigh_send_add_to_client(zvrf, zvni->vni, ip,
+					     macaddr, 0);
 }
 
 /*

commit b3ac7eba0ddecb538adf316dd5a7b154f89a17ff

zebra: Ensure locally-attached neighbors are flagged correctly
Ensure that when a new locally-attached neighbor is learnt, it is
flagged correctly.

Fixes: c4771937e6699ea8ea60ef68d18b6ddebf00a215
Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>
Reviewed-by:   Mitesh Kanjariya <mitesh@cumulusnetworks.com>
Reviewed-by:   Donald Sharp <sharpd@cumulusnetworks.com>

Ticket: CM-17621, CM-17608
Reviewed By: CCR-6618
Testing Done: Manual tests, selected tests in bgp_evpn_test.py

zebra/zebra_vxlan.c
@@ -2985,6 +2985,8 @@ int zebra_vxlan_local_neigh_add_update(struct interface *ifp,
 		{
 			UNSET_FLAG(n->flags, ZEBRA_NEIGH_REMOTE);
 			n->r_vtep_ip.s_addr = 0;
+			SET_FLAG(n->flags, ZEBRA_NEIGH_LOCAL);
+			n->ifindex = ifp->ifindex;
 		}
 	} else {
 		n = zvni_neigh_add(zvni, ip, macaddr);
@@ -2996,6 +2998,9 @@ int zebra_vxlan_local_neigh_add_update(struct interface *ifp,
 				ifp->name, ifp->ifindex, zvni->vni);
 			return -1;
 		}
+		/* Set "local" forwarding info. */
+		SET_FLAG(n->flags, ZEBRA_NEIGH_LOCAL);
+		n->ifindex = ifp->ifindex;
 	}
 
 	/* Before we program this in BGP, we need to check if MAC is locally