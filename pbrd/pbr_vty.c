51ee40155c3088ec0769af64c0fa285e4b30fd99
Donald Sharp <sharpd@cumulusnetworks.com>
2018-03-26 12:37:43 -0400

    pbrd: Indicate command failure a bit better
    When a command is attempted to be configed and it
    fails to be installed, indicate via vtysh return
    codes that we did not accept the command
    
    Ticket: CM-20216
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pbrd/pbr_vty.c
+++ pbrd/pbr_vty.c
@@ -154,7 +154,7 @@ DEFPY(pbr_map_nexthop_group, pbr_map_nexthop_group_cmd,
 	if (pbrms->nhg) {
 		vty_out(vty,
 			"A `set nexthop XX` command already exists, please remove that first\n");
-		return CMD_WARNING;
+		return CMD_WARNING_CONFIG_FAILED;
 	}
 
 	nhgc = nhgc_find(name);
@@ -171,14 +171,14 @@ DEFPY(pbr_map_nexthop_group, pbr_map_nexthop_group_cmd,
 			vty_out(vty,
 				"Nexthop Group specified: %s does not exist to remove",
 				name);
-			return CMD_WARNING;
+			return CMD_WARNING_CONFIG_FAILED;
 		}
 	} else {
 		if (pbrms->nhgrp_name) {
 			if (strcmp(name, pbrms->nhgrp_name) != 0) {
 				vty_out(vty,
 					"Please delete current nexthop group before modifying current one");
-				return CMD_WARNING;
+				return CMD_WARNING_CONFIG_FAILED;
 			}
 
 			return CMD_SUCCESS;
@@ -209,7 +209,7 @@ DEFPY(pbr_map_nexthop, pbr_map_nexthop_cmd,
 	if (pbrms->nhgrp_name) {
 		vty_out(vty,
 			"Please unconfigure the nexthop group before adding an individual nexthop");
-		return CMD_WARNING;
+		return CMD_WARNING_CONFIG_FAILED;
 	}
 
 	if (name)
@@ -219,7 +219,7 @@ DEFPY(pbr_map_nexthop, pbr_map_nexthop_cmd,
 
 	if (!vrf) {
 		vty_out(vty, "Specified: %s is non-existent\n", name);
-		return CMD_WARNING;
+		return CMD_WARNING_CONFIG_FAILED;
 	}
 
 	memset(&nhop, 0, sizeof(nhop));
@@ -234,7 +234,7 @@ DEFPY(pbr_map_nexthop, pbr_map_nexthop_cmd,
 				vty_out(vty,
 					"Specified Intf %s does not exist in vrf: %s\n",
 					intf, vrf->name);
-				return CMD_WARNING;
+				return CMD_WARNING_CONFIG_FAILED;
 			}
 		} else
 			nhop.type = NEXTHOP_TYPE_IPV4;
@@ -247,7 +247,7 @@ DEFPY(pbr_map_nexthop, pbr_map_nexthop_cmd,
 				vty_out(vty,
 					"Specified Intf %s does not exist in vrf: %s\n",
 					intf, vrf->name);
-				return CMD_WARNING;
+				return CMD_WARNING_CONFIG_FAILED;
 			}
 		} else
 			nhop.type = NEXTHOP_TYPE_IPV6;
@@ -260,7 +260,7 @@ DEFPY(pbr_map_nexthop, pbr_map_nexthop_cmd,
 
 		if (no) {
 			vty_out(vty, "No nexthops to delete");
-			return CMD_WARNING;
+			return CMD_WARNING_CONFIG_FAILED;
 		}
 
 		pbrms->nhg = nexthop_group_new();
@@ -280,7 +280,7 @@ DEFPY(pbr_map_nexthop, pbr_map_nexthop_cmd,
 		if (pbrms->nhg->nexthop) {
 			vty_out(vty,
 				"If you would like more than one nexthop please use nexthop-groups");
-			return CMD_WARNING;
+			return CMD_WARNING_CONFIG_FAILED;
 		}
 
 		/* must be adding new nexthop since !no and !nexthop_exists */
