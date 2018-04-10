d3b21fef2743f48ec9769afd709dd766a153d9a0
Donald Sharp <sharpd@cumulusnetworks.com>
2018-04-10 15:54:35 -0400

    pbrd: Use proper decode function for interface
    Use a proper decode function for a interface state change.
    
    Ticket: CM-20489
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pbrd/pbr_zebra.c
+++ pbrd/pbr_zebra.c
@@ -50,17 +50,6 @@ struct zclient *zclient = NULL;
 /* For registering threads. */
 extern struct thread_master *master;
 
-static struct interface *zebra_interface_if_lookup(struct stream *s)
-{
-	char ifname_tmp[INTERFACE_NAMSIZ];
-
-	/* Read interface name. */
-	stream_get(ifname_tmp, s, INTERFACE_NAMSIZ);
-
-	/* And look it up. */
-	return if_lookup_by_name(ifname_tmp, VRF_DEFAULT);
-}
-
 struct pbr_interface *pbr_if_new(struct interface *ifp)
 {
 	struct pbr_interface *pbr_ifp;
@@ -145,7 +134,7 @@ static int interface_state_up(int command, struct zclient *zclient,
 			      zebra_size_t length, vrf_id_t vrf_id)
 {
 
-	zebra_interface_if_lookup(zclient->ibuf);
+	zebra_interface_state_read(zclient->ibuf, vrf_id);
 
 	return 0;
 }
