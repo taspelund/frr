5ac0136a237cbe0a81ec76b984bf4d090aee29a2
Quentin Young <qlyoung@cumulusnetworks.com>
2018-06-21 22:02:13 +0000

    lib, isisd: stylin'
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- isisd/isisd.c
+++ isisd/isisd.c
@@ -2056,12 +2056,14 @@ int isis_config_write(struct vty *vty)
 			if (area->area_passwd.type
 			    == ISIS_PASSWD_TYPE_HMAC_MD5) {
 				if (host.obfuscate)
-					caesar(true, (char *) area->area_passwd.passwd,
+					caesar(true,
+					       (char *)area->area_passwd.passwd,
 					       ISIS_PASSWD_OBFUSCATION_KEY);
 				vty_out(vty, " area-password md5 %s",
 					area->area_passwd.passwd);
 				if (host.obfuscate)
-					caesar(false, (char *) area->area_passwd.passwd,
+					caesar(false,
+					       (char *)area->area_passwd.passwd,
 					       ISIS_PASSWD_OBFUSCATION_KEY);
 				if (CHECK_FLAG(area->area_passwd.snp_auth,
 					       SNP_AUTH_SEND)) {
@@ -2078,12 +2080,14 @@ int isis_config_write(struct vty *vty)
 			} else if (area->area_passwd.type
 				   == ISIS_PASSWD_TYPE_CLEARTXT) {
 				if (host.obfuscate)
-					caesar(true, (char *) area->area_passwd.passwd,
+					caesar(true,
+					       (char *)area->area_passwd.passwd,
 					       ISIS_PASSWD_OBFUSCATION_KEY);
 				vty_out(vty, " area-password clear %s",
 					area->area_passwd.passwd);
 				if (host.obfuscate)
-					caesar(false, (char *) area->area_passwd.passwd,
+					caesar(false,
+					       (char *)area->area_passwd.passwd,
 					       ISIS_PASSWD_OBFUSCATION_KEY);
 				if (CHECK_FLAG(area->area_passwd.snp_auth,
 					       SNP_AUTH_SEND)) {
@@ -2101,13 +2105,16 @@ int isis_config_write(struct vty *vty)
 			if (area->domain_passwd.type
 			    == ISIS_PASSWD_TYPE_HMAC_MD5) {
 				if (host.obfuscate)
-					caesar(true, (char *) area->domain_passwd.passwd,
+					caesar(true,
+					       (char *)area->domain_passwd
+						       .passwd,
 					       ISIS_PASSWD_OBFUSCATION_KEY);
 				vty_out(vty, " domain-password md5 %s",
 					area->domain_passwd.passwd);
 				if (host.obfuscate)
-					caesar(false, (char *)
-					       area->domain_passwd.passwd,
+					caesar(false,
+					       (char *)area->domain_passwd
+						       .passwd,
 					       ISIS_PASSWD_OBFUSCATION_KEY);
 				if (CHECK_FLAG(area->domain_passwd.snp_auth,
 					       SNP_AUTH_SEND)) {
--- lib/command.c
+++ lib/command.c
@@ -501,30 +501,30 @@ static char *zencrypt(const char *passwd)
 
 char *caesar(bool encrypt, char *text, const char *key)
 {
-        size_t kl = strlen(key);
-        size_t tl = strlen(text);
-        int16_t w[tl + 1];
+	size_t kl = strlen(key);
+	size_t tl = strlen(text);
+	int16_t w[tl + 1];
 
-        for (size_t i = 0; i < tl; ++i)
-                if (!(text[i] >= 33 && text[i] <= 126))
-                        return NULL;
+	for (size_t i = 0; i < tl; ++i)
+		if (!(text[i] >= 33 && text[i] <= 126))
+			return NULL;
 
-        for (size_t i = 0; i < kl; ++i)
-                if (!(key[i] >= 33 && key[i] <= 126))
-                        return NULL;
+	for (size_t i = 0; i < kl; ++i)
+		if (!(key[i] >= 33 && key[i] <= 126))
+			return NULL;
 
-        for (size_t i = 0; i < tl; ++i) {
-                w[i] = text[i];
-                w[i] += -33 + (2 * !!encrypt - 1) * key[i % kl];
-                w[i] = MOD((w[i]), (127 - 33)) + 33;
-        }
+	for (size_t i = 0; i < tl; ++i) {
+		w[i] = text[i];
+		w[i] += -33 + (2 * !!encrypt - 1) * key[i % kl];
+		w[i] = MOD((w[i]), (127 - 33)) + 33;
+	}
 
-        for (size_t i = 0; i < tl; i++)
-                text[i] = w[i];
+	for (size_t i = 0; i < tl; i++)
+		text[i] = w[i];
 
-        w[tl] = 0x00;
+	w[tl] = 0x00;
 
-        return text;
+	return text;
 }
 
 /* This function write configuration of this host. */
@@ -2037,11 +2037,10 @@ DEFUN (service_password_encrypt,
 	return CMD_SUCCESS;
 }
 
-DEFUN (service_password_obfuscate,
-       service_password_obfuscate_cmd,
-       "[no] service password-obfuscation",
-       "Set up miscellaneous service\n"
-       "Obfuscate unencrypted passwords\n")
+DEFUN(service_password_obfuscate, service_password_obfuscate_cmd,
+      "[no] service password-obfuscation",
+      "Set up miscellaneous service\n"
+      "Obfuscate unencrypted passwords\n")
 {
 	host.obfuscate = !strmatch(argv[0]->text, "no");
 
