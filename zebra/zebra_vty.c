2543959f9b77d6da2ea0316579ed710536b2886f
Don Slice <dslice@cumulusnetworks.com>
2018-07-17 14:20:52 +0000

    zebra: remove default vrf output for kernel vrf ipv6 blackhole default
    It was reported that "show ipv6 route vrf <vrfname>", "show ipv6 route
    vrf <vrfname> ::/0 " or "show ipv6 route vrf <vrfname> json" all
    displayed that the nexthop was in the default vrf.  This was because
    the kernel netlink messages would supply the RTA_OIF of the loopback
    interface for the kernel-created default route for the vrf, where ipv4
    did not supply any RTA_OIF.  This fix suppresses the display if the
    nexthop and route entry are in different vrfs and the nexthop is
    NEXTHOP_TYPE_BLACKHOLE.
    
    Ticket: CM-21722
    Signed-off-by: Don Slice <dslice@cumulusnetworks.com>

--- zebra/zebra_vty.c
+++ zebra/zebra_vty.c
@@ -1126,7 +1126,8 @@ static void vty_show_ip_route_detail(struct vty *vty, struct route_node *rn,
 				break;
 			}
 
-			if (re->vrf_id != nexthop->vrf_id) {
+			if ((re->vrf_id != nexthop->vrf_id)
+			     && !NEXTHOP_TYPE_BLACKHOLE) {
 				struct vrf *vrf =
 					vrf_lookup_by_id(nexthop->vrf_id);
 
@@ -1336,7 +1337,8 @@ static void vty_show_ip_route(struct vty *vty, struct route_node *rn,
 				break;
 			}
 
-			if (nexthop->vrf_id != re->vrf_id) {
+			if ((nexthop->vrf_id != re->vrf_id)
+			     && !NEXTHOP_TYPE_BLACKHOLE) {
 				struct vrf *vrf =
 					vrf_lookup_by_id(nexthop->vrf_id);
 
@@ -1489,7 +1491,8 @@ static void vty_show_ip_route(struct vty *vty, struct route_node *rn,
 			break;
 		}
 
-		if (nexthop->vrf_id != re->vrf_id) {
+		if ((nexthop->vrf_id != re->vrf_id)
+		     && !NEXTHOP_TYPE_BLACKHOLE) {
 			struct vrf *vrf = vrf_lookup_by_id(nexthop->vrf_id);
 
 			if (vrf)
