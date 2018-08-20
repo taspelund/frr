eca818574155081770d024d9a76d4c90f7d886bb
Donald Sharp <sharpd@cumulusnetworks.com>
2018-08-20 08:52:36 -0400

    lib: Convert to flog_warn in agentx.c
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- lib/lib_errors.c
+++ lib/lib_errors.c
@@ -21,6 +21,18 @@
 #include "lib_errors.h"
 
 /* clang-format off */
+static struct ferr_ref ferr_lib_warn[] = {
+	{
+		.code = LIB_WARN_SNMP,
+		.title = "SNMP has discovered a warning",
+		.description = "The SNMP AgentX library has returned a warning that we should report to the end user",
+		.suggestion = "Gather Log data and open an Issue.",
+	},
+	{
+		.code = END_FERR,
+	},
+};
+
 static struct ferr_ref ferr_lib_err[] = {
 	{
 		.code = LIB_ERR_PRIVILEGES,
@@ -115,4 +127,5 @@ static struct ferr_ref ferr_lib_err[] = {
 void lib_error_init(void)
 {
 	ferr_ref_add(ferr_lib_err);
+	ferr_ref_add(ferr_lib_warn);
 }
