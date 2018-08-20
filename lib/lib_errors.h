1e997efd74312cc98b1276058d31ee5557141a9c
Donald Sharp <sharpd@cumulusnetworks.com>
2018-08-20 09:27:30 -0400

    lib: Convert stream.c to use new error code subsystem
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- lib/lib_errors.h
+++ lib/lib_errors.h
@@ -39,6 +39,7 @@ enum lib_ferr_refs {
 	LIB_ERR_ZMQ,
 	LIB_ERR_UNAVAILABLE,
 	LIB_WARN_SNMP,
+	LIB_WARN_STREAM,
 };
 
 extern void lib_error_init(void);
--- lib/stream.c
+++ lib/stream.c
@@ -27,6 +27,7 @@
 #include "network.h"
 #include "prefix.h"
 #include "log.h"
+#include "lib_errors.h"
 
 DEFINE_MTYPE_STATIC(LIB, STREAM, "Stream")
 DEFINE_MTYPE_STATIC(LIB, STREAM_DATA, "Stream data")
@@ -53,7 +54,8 @@ DEFINE_MTYPE_STATIC(LIB, STREAM_FIFO, "Stream FIFO")
  * using stream_put..._at() functions.
  */
 #define STREAM_WARN_OFFSETS(S)                                                 \
-	zlog_warn("&(struct stream): %p, size: %lu, getp: %lu, endp: %lu\n",   \
+	flog_warn(LIB_WARN_STREAM,                                             \
+		  "&(struct stream): %p, size: %lu, getp: %lu, endp: %lu\n",   \
 		  (void *)(S), (unsigned long)(S)->size,                       \
 		  (unsigned long)(S)->getp, (unsigned long)(S)->endp)
 
@@ -65,26 +67,27 @@ DEFINE_MTYPE_STATIC(LIB, STREAM_FIFO, "Stream FIFO")
 		assert(ENDP_VALID(S, (S)->endp));                              \
 	} while (0)
 
-#define STREAM_BOUND_WARN(S, WHAT)					\
-	do {								\
-		zlog_warn("%s: Attempt to %s out of bounds", __func__,	\
-			  (WHAT));					\
-		STREAM_WARN_OFFSETS(S);					\
-		assert(0);						\
+#define STREAM_BOUND_WARN(S, WHAT)                                             \
+	do {                                                                   \
+		flog_warn(LIB_WARN_STREAM, "%s: Attempt to %s out of bounds",  \
+			  __func__, (WHAT));                                   \
+		STREAM_WARN_OFFSETS(S);                                        \
+		assert(0);                                                     \
 	} while (0)
 
-#define STREAM_BOUND_WARN2(S, WHAT)					\
-	do {								\
-		zlog_warn("%s: Attempt to %s out of bounds", __func__,	\
-			  (WHAT));					\
-		STREAM_WARN_OFFSETS(S);					\
+#define STREAM_BOUND_WARN2(S, WHAT)                                            \
+	do {                                                                   \
+		flog_warn(LIB_WARN_STREAM, "%s: Attempt to %s out of bounds",  \
+			  __func__, (WHAT));                                   \
+		STREAM_WARN_OFFSETS(S);                                        \
 	} while (0)
 
 /* XXX: Deprecated macro: do not use */
 #define CHECK_SIZE(S, Z)                                                       \
 	do {                                                                   \
 		if (((S)->endp + (Z)) > (S)->size) {                           \
-			zlog_warn(                                             \
+			flog_warn(                                             \
+				LIB_WARN_STREAM,                               \
 				"CHECK_SIZE: truncating requested size %lu\n", \
 				(unsigned long)(Z));                           \
 			STREAM_WARN_OFFSETS(S);                                \
@@ -967,8 +970,8 @@ ssize_t stream_read_try(struct stream *s, int fd, size_t size)
 	/* Error: was it transient (return -2) or fatal (return -1)? */
 	if (ERRNO_IO_RETRY(errno))
 		return -2;
-	zlog_warn("%s: read failed on fd %d: %s", __func__, fd,
-		  safe_strerror(errno));
+	flog_err(LIB_ERR_SOCKET, "%s: read failed on fd %d: %s", __func__, fd,
+		 safe_strerror(errno));
 	return -1;
 }
 
@@ -998,8 +1001,8 @@ ssize_t stream_recvfrom(struct stream *s, int fd, size_t size, int flags,
 	/* Error: was it transient (return -2) or fatal (return -1)? */
 	if (ERRNO_IO_RETRY(errno))
 		return -2;
-	zlog_warn("%s: read failed on fd %d: %s", __func__, fd,
-		  safe_strerror(errno));
+	flog_err(LIB_ERR_SOCKET, "%s: read failed on fd %d: %s", __func__, fd,
+		 safe_strerror(errno));
 	return -1;
 }
 
