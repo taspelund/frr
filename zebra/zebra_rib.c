a22a24a44ab5b6cfa2b2d11713d97ce3de83c204
Donald Sharp <sharpd@cumulusnetworks.com>
2019-01-30 09:50:38 -0500

    cumulus: Fixup
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/zebra_rib.c
+++ zebra/zebra_rib.c
@@ -1942,6 +1942,14 @@ static void rib_process_result(struct zebra_dplane_ctx *ctx)
 			 * old_re in this case is used for nothing
 			 * more than knowing whom to contact if necessary.
 			 */
+			if (old_re) {
+				UNSET_FLAG(old_re->status, ROUTE_ENTRY_FAILED);
+				UNSET_FLAG(old_re->status,
+					   ROUTE_ENTRY_INSTALLED);
+			}
+			/* Update zebra nexthop FIB flag for each
+			 * nexthop that was installed.
+			 */
 			for (ALL_NEXTHOPS_PTR(dplane_ctx_get_ng(ctx),
 					      ctx_nexthop)) {
 
