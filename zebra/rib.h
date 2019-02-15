ef73d318683f07e95e62019c40c89ef53573d054
Donald Sharp <sharpd@cumulusnetworks.com>
2019-02-15 11:57:22 -0500

    zebra: Reorder `struct route_entry` to reduce size
    Reduce the size of the data structure from 88 bytes to 80 bytes
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/rib.h
+++ zebra/rib.h
@@ -95,9 +95,6 @@ struct route_entry {
 	/* Type fo this route. */
 	int type;
 
-	/* Source protocol instance */
-	unsigned short instance;
-
 	/* VRF identifier. */
 	vrf_id_t vrf_id;
 
@@ -110,9 +107,6 @@ struct route_entry {
 	/* MTU */
 	uint32_t mtu;
 	uint32_t nexthop_mtu;
-
-	/* Distance. */
-	uint8_t distance;
 
 	/* Flags of this route.
 	 * This flag's definition is in lib/zebra.h ZEBRA_FLAG_* and is exposed
@@ -142,6 +136,12 @@ struct route_entry {
 
 	/* Sequence value incremented for each dataplane operation */
 	uint32_t dplane_sequence;
+
+	/* Source protocol instance */
+	uint16_t instance;
+
+	/* Distance. */
+	uint8_t distance;
 };
 
 #define RIB_SYSTEM_ROUTE(R) RSYSTEM_ROUTE((R)->type)
