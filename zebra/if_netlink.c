cfd6034aa4b9cb12667279dba5797d20ccca4fc4
Stephen Worley <sworley@cumulusnetworks.com>
2019-03-06 14:08:13 -0500

    zebra: Add kernel condition check to see if it supports nexthops
    Added a check on startup for determining if the kernel supports
    nexthop objects. It sets an appropriate bool on the zebra namespace
    struct.
    
    Signed-off-by: Stephen Worley <sworley@cumulusnetworks.com>

--- zebra/if_netlink.c
+++ zebra/if_netlink.c
@@ -1454,9 +1454,17 @@ void interface_list(struct zebra_ns *zns)
 	 * so we need to get the nexthop info
 	 * from the kernel before we can do that
 	 */
-	// TODO: Mark a failure with boolean on dataplane provider to indicate
-	// it is a kenrel that doesn't support nh objects.
-	netlink_nexthop_read(zns);
+
+	if (netlink_nexthop_read(zns)) {
+		/* If the nexthop read fails, assume the kernel
+		 * cannot handle nexthop objects.
+		 */
+		zlog_debug("Nexthop objects disabled on this kernel");
+		zns->supports_nh = false;
+	} else {
+		zns->supports_nh = true;
+	}
+
 	interface_addr_lookup_netlink(zns);
 }
 
