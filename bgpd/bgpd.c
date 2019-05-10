0318d19d1fbf9fb180049b91b98e60ee5badc87c
David Lamparter <equinox@diac24.net>
2019-05-10 15:31:04 +0200

    bgpd: add instance delete & config write hooks
    Both of these hooks are necessary for proper operation of extensions
    that need to latch on to a particular instance.
    
    - without the delete hook, it's impossible to get rid of stale
      references, leading to crashes with invalid instance pointers.
    - the config-write hook is necessary because per-instance config needs
      to be written inside the "router bgp" block to have the appropriate
      context; adding a separate config node can't do that.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -94,6 +94,10 @@ DEFINE_MTYPE_STATIC(BGPD, BGP_EVPN_INFO, "BGP EVPN instance information");
 DEFINE_QOBJ_TYPE(bgp_master)
 DEFINE_QOBJ_TYPE(bgp)
 DEFINE_QOBJ_TYPE(peer)
+DEFINE_HOOK(bgp_inst_delete, (struct bgp *bgp), (bgp))
+DEFINE_HOOK(bgp_inst_config_write,
+		(struct bgp *bgp, struct vty *vty),
+		(bgp, vty))
 
 /* BGP process wide configuration.  */
 static struct bgp_master bgp_master;
@@ -3270,6 +3274,9 @@ int bgp_delete(struct bgp *bgp)
 	int i;
 
 	assert(bgp);
+
+	hook_call(bgp_inst_delete, bgp);
+
 	THREAD_OFF(bgp->t_startup);
 	THREAD_OFF(bgp->t_maxmed_onstartup);
 	THREAD_OFF(bgp->t_update_delay);
@@ -7756,6 +7763,8 @@ int bgp_config_write(struct vty *vty)
 
 		/* EVPN configuration.  */
 		bgp_config_write_family(vty, bgp, AFI_L2VPN, SAFI_EVPN);
+
+		hook_call(bgp_inst_config_write, bgp, vty);
 
 #if ENABLE_BGP_VNC
 		bgp_rfapi_cfg_write(vty, bgp);
