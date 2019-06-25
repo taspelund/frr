83a889de421a0c552dfa3e669532d225d3a1640d
Donald Sharp <sharpd@cumulusnetworks.com>
2019-06-25 17:07:30 -0400

    zebra: Push VRF_DEFAULT outside of import table code
    The import table code assumes that they will only work
    in the default vrf.  This is ok, but we should push the
    vrf_id and zvrf to be passed in instead of just using
    VRF_DEFAULT.
    
    This will allow us to fix a couple of things:
    
    1) A bug in import where we are not creating the
    route entry with the appropriate table so the imported
    entry is showing up in the wrong spot.
    
    2) In the future allow `ip import-table X` to become
    vrf aware very easily.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/zebra_vty.c
+++ zebra/zebra_vty.c
@@ -2442,7 +2442,7 @@ static int zebra_ip_config(struct vty *vty)
 {
 	int write = 0;
 
-	write += zebra_import_table_config(vty);
+	write += zebra_import_table_config(vty, VRF_DEFAULT);
 
 	return write;
 }
@@ -2489,7 +2489,8 @@ DEFUN (ip_zebra_import_table_distance,
 		return CMD_WARNING;
 	}
 
-	ret = zebra_import_table(AFI_IP, table_id, distance, rmap, 1);
+	ret = zebra_import_table(AFI_IP, VRF_DEFAULT, table_id,
+				 distance, rmap, 1);
 	if (rmap)
 		XFREE(MTYPE_ROUTE_MAP_NAME, rmap);
 
@@ -2580,10 +2581,10 @@ DEFUN (no_ip_zebra_import_table,
 		return CMD_WARNING;
 	}
 
-	if (!is_zebra_import_table_enabled(AFI_IP, table_id))
+	if (!is_zebra_import_table_enabled(AFI_IP, VRF_DEFAULT, table_id))
 		return CMD_SUCCESS;
 
-	return (zebra_import_table(AFI_IP, table_id, 0, NULL, 0));
+	return (zebra_import_table(AFI_IP, VRF_DEFAULT, table_id, 0, NULL, 0));
 }
 
 static int config_write_protocol(struct vty *vty)
