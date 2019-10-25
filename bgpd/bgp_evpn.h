9c1a52f398714e8d98ddebedf706bac4123742a5
Chirag Shah <chirag@cumulusnetworks.com>
2019-10-24 18:36:39 -0700

    Revert "bgpd: Adding new bgp evpn cli's for ip-prefix lookup"
    Json output format changes for some of the existing clis as well.
    The patch needs some more formatting for json output format.
    revertting for now.
    
    This reverts commit 2a8716dc8ded87f17f0793869177703bf176fa88.
    
    Signed-off-by: Chirag Shah <chirag@cumulusnetworks.com>

--- bgpd/bgp_evpn.h
+++ bgpd/bgp_evpn.h
@@ -192,6 +192,5 @@ extern void bgp_evpn_cleanup_on_disable(struct bgp *bgp);
 extern void bgp_evpn_cleanup(struct bgp *bgp);
 extern void bgp_evpn_init(struct bgp *bgp);
 extern void update_advertise_vrf_routes(struct bgp *bgp_vrf);
-extern int bgp_evpn_get_type5_prefixlen(struct prefix *pfx);
 
 #endif /* _QUAGGA_BGP_EVPN_H */
