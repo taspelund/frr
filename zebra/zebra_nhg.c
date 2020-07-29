57a2e4f39f0f8f1284242b5f73ae2caa8049baf3
Stephen Worley <sworley@cumulusnetworks.com>
2020-07-29 13:11:37 -0400

    zebra,sharpd: checkpatch fixes
    Check patches fixes for NHG API pathes.
    
    Signed-off-by: Stephen Worley <sworley@cumulusnetworks.com>

--- zebra/zebra_nhg.c
+++ zebra/zebra_nhg.c
@@ -68,7 +68,7 @@ static struct nhg_backup_info *
 nhg_backup_copy(const struct nhg_backup_info *orig);
 
 /* Helper function for getting the next allocatable ID */
-static uint32_t nhg_get_next_id()
+static uint32_t nhg_get_next_id(void)
 {
 	while (1) {
 		id_counter++;
