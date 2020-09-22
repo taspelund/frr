a98f3b9c5cdd8a5188fc46f50e982a21dfb357bb
Anuradha Karuppiah <anuradhak@cumulusnetworks.com>
2020-09-22 06:16:52 -0700

    zebra: del/add remote mac if there is a change from es->non-es dst and vicevera
    This is needed as kernel currently doesn't allow a mac replace if the dst
    changes from a L2NHG to a single-VTEP and viceversa.
    
    Ticket: CM-31561
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

--- zebra/zebra_evpn_mac.c
+++ zebra/zebra_evpn_mac.c
@@ -1868,6 +1868,8 @@ int process_mac_remote_macip_add(zebra_evpn_t *zevpn, struct zebra_vrf *zvrf,
 	esi_t *old_esi;
 	bool old_static = false;
 	zebra_mac_t *mac;
+	bool old_es_present;
+	bool new_es_present;
 
 	sticky = !!CHECK_FLAG(flags, ZEBRA_MACIP_TYPE_STICKY);
 	remote_gw = !!CHECK_FLAG(flags, ZEBRA_MACIP_TYPE_GW);
@@ -1948,7 +1950,16 @@ int process_mac_remote_macip_add(zebra_evpn_t *zevpn, struct zebra_vrf *zvrf,
 				return -1;
 			}
 
+			old_es_present = !!mac->es;
 			zebra_evpn_es_mac_ref(mac, esi);
+			new_es_present = !!mac->es;
+			/* XXX - dataplane is curently not able to handle a MAC
+			 * replace if the destination changes from L2-NHG to single
+			 * VTEP and vice-versa. So delete the old entry and
+			 * re-install
+			 */
+			if (old_es_present != new_es_present)
+				zebra_evpn_rem_mac_uninstall(zevpn, mac, false);
 		}
 
 		/* Check MAC's curent state is local (this is the case
