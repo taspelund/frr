aefc58d19271089f9ade9d4ffcac7d719fcb5342
Trey Aspelund <taspelund@nvidia.com>
2022-06-27 23:20:33 +0000

    bgpd: always keepalive/hold intervals in json
    `show bgp neighbors <peer> json` was only displaying the configured
    keepalive and holdtime intervals when they differed from the default
    values.  Since default config is still config, let's make sure these
    values are always displayed.
    
    Signed-off-by: Trey Aspelund <taspelund@nvidia.com>

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -12718,6 +12718,14 @@ static void bgp_show_peer(struct vty *vty, struct peer *p, bool use_json,
 					    + (tm.tm_hour * 3600000));
 
 		/* Configured timer values. */
+		json_object_int_add(json_neigh,
+				    "bgpTimerConfiguredHoldTimeMsecs",
+				    p->holdtime ? p->holdtime * 1000
+						: bgp->default_holdtime * 1000);
+		json_object_int_add(
+			json_neigh, "bgpTimerConfiguredKeepAliveIntervalMsecs",
+			p->keepalive ? p->keepalive * 1000
+				     : bgp->default_keepalive * 1000);
 		json_object_int_add(json_neigh, "bgpTimerHoldTimeMsecs",
 				    p->v_holdtime * 1000);
 		json_object_int_add(json_neigh,
@@ -12736,25 +12744,6 @@ static void bgp_show_peer(struct vty *vty, struct peer *p, bool use_json,
 					    p->tcp_mss);
 			json_object_int_add(json_neigh, "bgpTcpMssSynced",
 					    sync_tcp_mss);
-		}
-
-		if (CHECK_FLAG(p->flags, PEER_FLAG_TIMER)) {
-			json_object_int_add(json_neigh,
-					    "bgpTimerConfiguredHoldTimeMsecs",
-					    p->holdtime * 1000);
-			json_object_int_add(
-				json_neigh,
-				"bgpTimerConfiguredKeepAliveIntervalMsecs",
-				p->keepalive * 1000);
-		} else if ((bgp->default_holdtime != SAVE_BGP_HOLDTIME)
-			   || (bgp->default_keepalive != SAVE_BGP_KEEPALIVE)) {
-			json_object_int_add(json_neigh,
-					    "bgpTimerConfiguredHoldTimeMsecs",
-					    bgp->default_holdtime);
-			json_object_int_add(
-				json_neigh,
-				"bgpTimerConfiguredKeepAliveIntervalMsecs",
-				bgp->default_keepalive);
 		}
 
 		/* Extended Optional Parameters Length for BGP OPEN Message */
